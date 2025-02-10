96c7dcbfd82a92f9a2ba3128a8a963e9c75d0c58
Fabian Braun <fsbraun@gmx.de>
2025-02-10 11:40:46 +0100

    fix: Copy plugins was broken  (#8135)
    * fix: test copy plugins
    
    * fix linting
    
    * Improved naming
    
    ---------
    
    Co-authored-by: Vinit Kumar <mail@vinitkumar.me>

--- cms/models/settingmodels.py
+++ cms/models/settingmodels.py
@@ -4,17 +4,27 @@ from django.utils.encoding import force_str
 from django.utils.translation import gettext_lazy as _
 
 
+class PlaceholderForeignKey(models.ForeignKey):
+    def run_checks(self, *args, **kwargs):
+        # User always has permission to change their own clipboard
+        return True
+
+
 class UserSettings(models.Model):
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         editable=False,
-        related_name='djangocms_usersettings',
+        related_name="djangocms_usersettings",
     )
-    language = models.CharField(_("Language"), max_length=10, choices=settings.LANGUAGES,
-                                help_text=_("The language for the admin interface and toolbar"))
-    clipboard = models.ForeignKey(
-        'cms.Placeholder',
+    language = models.CharField(
+        _("Language"),
+        max_length=10,
+        choices=settings.LANGUAGES,
+        help_text=_("The language for the admin interface and toolbar"),
+    )
+    clipboard = PlaceholderForeignKey(
+        "cms.Placeholder",
         on_delete=models.CASCADE,
         blank=True,
         null=True,
@@ -22,12 +32,13 @@ class UserSettings(models.Model):
     )
 
     class Meta:
-        verbose_name = _('user setting')
-        verbose_name_plural = _('user settings')
-        app_label = 'cms'
+        verbose_name = _("user setting")
+        verbose_name_plural = _("user settings")
+        app_label = "cms"
 
     def __str__(self):
         return force_str(self.user)
 
     def has_placeholder_change_permission(self, user):
+        # User always has permission to change their own clipboard
         return True
